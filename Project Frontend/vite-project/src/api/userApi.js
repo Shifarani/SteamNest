afd3423b567a14372797089e27a84da2a757ab61
Shifa Rani <shifarani.bcse2024@huroorkee.ac.in>
2026-08-18 14:33:50 +0530

    fix current user api

--- Project Frontend/vite-project/src/api/userApi.js
+++ Project Frontend/vite-project/src/api/userApi.js
@@ -1,12 +1,12 @@
 import axiosInstance from "./axiosInstance";
 
 export const getCurrentUser = async () => {
-  const response = await API.get("/current-user");
+  const response = await axiosInstance.get("/current-user");
   return response.data;
 };
 
 export const logoutUser = async () => {
-  const response = await API.post("/logout");
+  const response = await axiosInstance.post("/logout");
   return response.data;
 };
 export const updateAvatar = async(formData)=>{
